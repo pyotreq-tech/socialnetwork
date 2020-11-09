6925e56a62eb4357a04431bdf58abb173e27aea9
piotrek_kp <piotrek.kp@gmail.com>
2020-11-09 18:00:49 +0100

    part6

--- src/OtherProfile.js
+++ src/OtherProfile.js
@@ -12,9 +12,7 @@ export default class OtherProfile extends React.Component {
         axios
             .get(`/api/user/${this.props.match.params.id}`)
             .then((response) => {
-                console.log("response:", response);
                 if (response.data.denied) {
-                    console.log("false");
                     this.props.history.push("/");
                 } else {
                     this.setState({
@@ -23,19 +21,19 @@ export default class OtherProfile extends React.Component {
                         profileimage: response.data.profileimage,
                         bio: response.data.bio,
                     });
-                    console.log("this state otherprofile: ", this.state);
                 }
             })
             .catch((err) => {
-                console.log("id: ", this.props);
                 console.log("error in obtaining individual user data: ", err);
             });
     }
     render() {
         return (
             <>
-                <h1>Hi, I am another profile</h1>
-                <h2>Coming up next</h2>
+                <img src={this.state.profileimage || "/empty-image.jpg"} />
+                <h3>{this.state.first}</h3>
+                <h3>{this.state.last}</h3>
+                <h3>{this.state.bio}</h3>
             </>
         );
     }
